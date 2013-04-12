2b0acc1b0fdb7137cc68f458a23ce6320000eff4
Akhil Verghese <akhil@Akhils-MacBook-Pro.local>
2013-04-12 15:41:58 +0530

    Initial Commit

---
+++ FootPressureMap/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  FootPressureMap
+//
+//  Created by Akhil Verghese on 4/12/13.
+//  Copyright (c) 2013 Akhil Verghese. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
